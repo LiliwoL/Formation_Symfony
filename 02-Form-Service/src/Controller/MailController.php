97cb0a26642ad24974a9f735fb3f683f03b3c8f2
LiliwoL <github@liliwol.fr>
2019-09-19 14:57:05 +0200

    Update 02 commentaires

--- 02-Form-Service/src/Controller/MailController.php
+++ 02-Form-Service/src/Controller/MailController.php
@@ -17,7 +17,16 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-
+/**
+ * Classe MailController
+ * 
+ * -> Injection de dépendances
+ * -> Entité
+ * -> Validation
+ * -> Formulaire dans le contrôleur
+ * -> Formulaire dans un FormType
+ * -> Vue
+ */
 class MailController extends Controller
 {
     /**
@@ -27,104 +36,118 @@ class MailController extends Controller
      */
     public function new(Request $request, \Swift_Mailer $mailer, ValidatorInterface $validator )
     {
-
         /*
-            Ici, utilisation de 2 services
+            Ici, utilisation de 3 services
             Ces services sont "demandés" en typant les paramètres de l'action du controller
+            C'est ce qu'on appelle l'AUTOWIRING
+            https://symfony.com/doc/current/service_container/autowiring.html
 
-            Ainsi, on demande le service Request et le service Mailer
+            Ainsi, on demande le service Request, le service Mailer et le service Validator
             https://symfony.com/doc/current/service_container.html#content_wrapper
         */
 
+        // ********************************************************
 
-        // Création d'un objet Mail (une Entity) et on lui affecte des valeurs de bases
-        $mail = new Mail();
-        $mail->setSender( "sendermail.com" );
-        $mail->setDest( "dest@mail.com" );
-        $mail->setObject( "Test mail" );
-        $mail->setBody( "Un message de test");
+        // ********** CREATION ET VALIDATION D'UNE ENTITE
+        // Création d'un objet Mail (une Entity) 
+        // et on lui affecte des valeurs de bases
+        $mailEntity = new Mail();
+        $mailEntity->setSender( "sender@mail.com" );
+        $mailEntity->setDest( "dest@mail.com" );
+        $mailEntity->setObject( "Test mail" );
+        $mailEntity->setBody( "Un message de test");
 
-        $errors = $validator->validate( $mail );
+        // Validation de l'entité
+        $errors = $validator->validate( $mailEntity );
 
+        // En présence d'erreur, renvoi d'une réponse avec les erreurs
         if ( count( $errors) > 0 )
         {
             $errorsString = (string) $errors;
 
             return new Response( $errorsString);
         }
+        // ********************************************************
 
 
+        // ********** CREATION D'UN FORMULAIRE DANS LE CONTRÔLEUR
+            // Uniquement pour un formulaire à usage unique
+            $formulaireUnique = $this->createFormBuilder( $mailEntity ) // On passe l'entité en paramètre
+                ->add('sender', EmailType::class,
+                    array(
+                        'required' => false,
+                        'label'  => 'Expéditeur: ',
+                    ))
+                ->add('dest', EmailType::class)
+                ->add('object', TextType::class)
+                ->add('body', TextareaType::class)
+                ->add('save', SubmitType::class, array('label' => 'Send Mail'))
+            ->getForm();
+            
+            // Gestion de ce formulaire
+            $formulaireUnique->handleRequest($request);
 
-        // Formulaire créé dans le controlleur --> uniquement pour un formulaire à usage unique
-        $form = $this->createFormBuilder( $mail )
-            ->add('sender', EmailType::class,
-                array(
-                    'required' => false,
-                    'label'  => 'Expéditeur: ',
-                ))
-            ->add('dest', EmailType::class)
-            ->add('object', TextType::class)
-            ->add('body', TextareaType::class)
-            ->add('save', SubmitType::class, array('label' => 'Send Mail'))
-        ->getForm();
+            // Validation
+            if ($formulaireUnique->isSubmitted() && $formulaireUnique->isValid()) 
+            {
+                // Mise à jour de l'entité avec les données du formulaire
+                // $formulaireUnique->getData()
+                $mailEntity = $formulaireUnique->getData();
 
+                // Création d'un message avec les données de l'entité
+                $message = (
+                        new \Swift_Message( $mailEntity->getObject() )
+                    )
+                    ->setFrom([ $mailEntity->getSender() => 'John Doe'])
+                    ->setTo([ $mailEntity->getDest() => 'A name'])
+                    ->setBody( $mailEntity->getBody() 
+                );
 
-        /*
-        $form->handleRequest($request);
+                // Envoi du message via le service SwiftMailer
+                $result = $mailer->send($message);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $mail = $form->getData();
-
-            // Create a message
-            $message = (
-            new \Swift_Message( $mail->getObject() ))
-                ->setFrom([ $mail->getSender() => 'John Doe'])
-                ->setTo([ $mail->getDest() => 'A name'])
-                ->setBody( $mail->getBody() )
-            ;
-
-            // Send the message
-            $result = $mailer->send($message);
-
-            return $this->redirectToRoute('mail_success');
-        }
-        */
-
-        // ##################################@
-
-
-        // Formulaire créé dans un fichier différent, réutilisable à différents endrpits de l'app
-        $mailForm = $this->createForm ( MailType::class, $mail );
+                return $this->redirectToRoute('mail_success');
+            }
+        // ********************************************************
 
 
+        // ********** CREATION D'UN FORMULAIRE ISSU DE FORM_TYPE
+            // Réutilisable à différents endrpits de l'app
+            $formulaireReutilisable = $this->createForm ( 
+                MailType::class,    // Classe du formulaire MailType
+                $mailEntity         // Entité à utiliser
+            );
 
-        $mailForm->handleRequest($request);
+            // Gestion de ce formulaire
+            $formulaireReutilisable->handleRequest($request);
 
-        if ($mailForm->isSubmitted() && $mailForm->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-                $mail = $mailForm->getData();
+            // Validation
+            if ($formulaireReutilisable->isSubmitted() && $formulaireReutilisable->isValid()) 
+            {
+                // $form->getData() holds the submitted values
+                // but, the original `$task` variable has also been updated
+                    $mailEntity = $formulaireReutilisable->getData();
 
-            // Create a message
-            $message = (
-                new \Swift_Message( $mail->getObject() ))
-                    ->setFrom([ $mail->getSender() => 'John Doe'])
-                    ->setTo([ $mail->getDest() => 'A name'])
-                    ->setBody( $mail->getBody() )
-            ;
+                // Create a message
+                $message = (
+                        new \Swift_Message( $mailEntity->getObject() )
+                    )
+                        ->setFrom([ $mailEntity->getSender() => 'John Doe'])
+                        ->setTo([ $mailEntity->getDest() => 'A name'])
+                        ->setBody( $mailEntity->getBody() )
+                ;
 
-            // Send the message
-            $result = $mailer->send($message);
+                // Envoi du message via le service SwiftMailer
+                $result = $mailer->send($message);
 
 
-            return $this->redirectToRoute('mail_success');
-        }
+                return $this->redirectToRoute('mail_success');
+            }
 
 
         return $this->render('mail/new.html.twig', array(
-            'form' => $mailForm->createView(),
+            'formulaireUnique' => $formulaireUnique->createView(),
+            'formulaireReutilisable' => $formulaireReutilisable->createView(),
             'variable' => "Une variable"
         ));
     }
